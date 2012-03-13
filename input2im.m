86b94527cdf02ed7dfae766d8d382bf3d4fb24fa
Andrew Burnie <a.burnie@sms.ed.ac.uk>
2012-03-13 17:46:49 +0000

    Fixed input2im properly...

--- input2im.m
+++ input2im.m
@@ -3,8 +3,8 @@ function [image] = input2im(input)
     image = zeros(480,640,6);
 
     inputPos = 1;
-    for c = 1:480
-       for r = 1:640          
+    for r = 1:480
+       for c = 1:640          
             image(r,c,:) = input(inputPos,:);
             inputPos = inputPos + 1;
         end
