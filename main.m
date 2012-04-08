fc26e601decde2e543e6022f9f27fb0ecefd860a
Andrew Burnie <a.burnie@sms.ed.ac.uk>
2012-04-08 14:45:32 +0100

    manoverlayed on each image in turn, bits vanish on sticky out bits of wall

--- main.m
+++ main.m
@@ -1,43 +1,51 @@
 function main()
 
     [bgDepths, bgIm] = getBackground;
-    [fgDepths, fgIm] = input2image(importdata('bindermat/xyzrgb_frame_0020.mat'));
-    
-    figure(1)
-    imshow(bgIm)
-    figure(2)
-    imshow(fgIm)
-    
-    newDepth = abs(bgDepths(:,:,3) - fgDepths(:,:,3));
-       
-    showable = newDepth-min(min(newDepth));
-    
-    showable = showable/max(max(showable));
 
-    figure(3)
-    imshow(showable);
-
-    figure(4)
-    hist(showable(:),200);
+    for i = 1:36
+        filename = ['bindermat/xyzrgb_frame_' sprintf('%04d', i) '.mat'];
     
-    better = showable .* double(showable<0.5);
-    better = better-min(min(better));
-    better = better/max(max(better));
-    
-    figure(5)
-    imshow(better);
-       
-    figure(6)
-    manInIm = zeros(480, 640, 3);
-    % put in background
-    bgBin = (showable<0.1) | (showable>0.3);
-    manInIm(:,:,1) = bgIm(:,:,1) .* double(bgBin);
-    manInIm(:,:,2) = bgIm(:,:,2) .* double(bgBin);
-    manInIm(:,:,3) = bgIm(:,:,3) .* double(bgBin);
-    
-    % put in foreground
-    manInIm(:,:,1) = manInIm(:,:,1) + (fgIm(:,:,1) .* double(~bgBin));
-    manInIm(:,:,2) = manInIm(:,:,2) + (fgIm(:,:,2) .* double(~bgBin));
-    manInIm(:,:,3) = manInIm(:,:,3) + (fgIm(:,:,3) .* double(~bgBin));
+        [fgDepths, fgIm] = input2image(importdata(filename));
         
-    imshow(manInIm)
+        %figure(1)
+        %imshow(bgIm)
+        %figure(2)
+        %imshow(fgIm)
+        
+        newDepth = abs(bgDepths(:,:,3) - fgDepths(:,:,3));
+           
+        showable = newDepth-min(min(newDepth));
+        
+        showable = showable/max(max(showable));
+
+        %figure(3)
+        %imshow(showable);
+
+        %figure(4)
+        %hist(showable(:),200);
+        
+        better = showable .* double(showable<0.5);
+        better = better-min(min(better));
+        better = better/max(max(better));
+        
+        %figure(5)
+        %imshow(better);
+           
+        %figure(6)
+        manInIm = zeros(480, 640, 3);
+        % put in background
+        bgBin = (showable<0.07) | (showable>0.3);
+        manInIm(:,:,1) = bgIm(:,:,1) .* double(bgBin);
+        manInIm(:,:,2) = bgIm(:,:,2) .* double(bgBin);
+        manInIm(:,:,3) = bgIm(:,:,3) .* double(bgBin);
+        
+        % put in foreground
+        manInIm(:,:,1) = manInIm(:,:,1) + (fgIm(:,:,1) .* double(~bgBin));
+        manInIm(:,:,2) = manInIm(:,:,2) + (fgIm(:,:,2) .* double(~bgBin));
+        manInIm(:,:,3) = manInIm(:,:,3) + (fgIm(:,:,3) .* double(~bgBin));
+           
+           
+        figure(i) 
+        imshow(manInIm)
+        
+end
