78a6f12953d96e19eb35cd93b8a2d0d206d1d4b0
Your Name <murraysettle@hotmail.co.uk>
2012-04-08 14:04:56 +0100

    New overlay

--- overlay.m
+++ overlay.m
@@ -4,13 +4,10 @@ function outimage = overlay(xyzrgb, mappedIm)
     [h, w, l] = size(mappedIm);
 
     outimage = zeros(a,b,l);
-    blackPixel = zeros(1,3);
 
     for i = 1 : a
         for j = 1 : b
             if mappedIm(i,j,1) == 0 & mappedIm(i,j,2) == 0 & mappedIm(i,j,3) == 0
-                outimage(i,j,:) = xyzrgb(i,j,4:6);
-            elseif xyzrgb(i,j,3) > 0
                 outimage(i,j,:) = xyzrgb(i,j,4:6);
             else
                 outimage(i,j,:) = mappedIm(i,j,:);
